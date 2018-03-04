1bb0df1240f22f3b60fd1fbec4d95c0c116afe56
ArinaGonchar2018 <arina.gonchar2013@yandex.ru>
2018-03-04 15:41:06 +0300

    Лабораторная работа №1. дисциплина: "Алгоритмы и структуры данных"

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите b:");
+            double b = Convert.ToDouble(Console.ReadLine());
+            double z1 = (Math.Sqrt(2 * b + 2 * Math.Sqrt(Math.Pow(b, 2) - 4))) / (Math.Sqrt(Math.Pow(b, 2) - 4) + b + 2);
+            Console.WriteLine("Результат:");
+            Console.WriteLine("z1={0}", z1);
+            double z2 = (1) / (Math.Sqrt(b + 2));
+            Console.WriteLine("Результат:");
+            Console.WriteLine("z2={0}", z2);
+            Console.ReadKey();
+        }
+    }
+}
