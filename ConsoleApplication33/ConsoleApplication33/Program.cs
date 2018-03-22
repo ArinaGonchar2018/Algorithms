696c4356a87be52561b301352eae66a13bdb0481
ArinaGonchar2018 <arina.gonchar2013@yandex.ru>
2018-03-22 09:06:32 +0300

    Лабораторная работа № 2.Задание №1.

--- ConsoleApplication33/ConsoleApplication33/Program.cs
+++ ConsoleApplication33/ConsoleApplication33/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApplication4
+namespace ConsoleApplication33
 {
     class Program
     {
@@ -21,39 +21,35 @@ namespace ConsoleApplication4
             else if (x > -6 & x < -4)
             {
                 y = -x / 2 - 2;
-
             }
             else if (x > -4 & x < 0)
             {
                 Console.WriteLine("Введите значение R");
                 string r = Console.ReadLine();
                 double R = Convert.ToDouble(r);
-                y = Math.Sqrt(4 - Math.Pow((x + 2), 2));
-
+                y = Math.Sqrt(4 - Math.Pow(x + 2, 2));
             }
             else if (x > 0 & x < 2)
             {
                 Console.WriteLine("Введите значение R");
                 string r = Console.ReadLine();
                 double R = Convert.ToDouble(r);
-                y = Math.Sqrt(1 - Math.Pow((x - 1), 2));
-
+                y = Math.Sqrt(1 - Math.Pow(x - 1, 2));
             }
             else if (x > 2 & x < 3)
+            {
                 y = -x + 2;
+            }
             else if (x > 3)
             {
-                Console.WriteLine("Значение находится вне пределов интервала");
+                Console.WriteLine("Значениe находятся вне пределов интервала");
             }
-
             else
             {
-                Console.WriteLine("Значение находится вне пределов интервала");
-
+                Console.WriteLine("Значениe находятся вне пределов интервала");
             }
             Console.WriteLine("y=" + y);
             Console.ReadKey();
-
         }
     }
 }
